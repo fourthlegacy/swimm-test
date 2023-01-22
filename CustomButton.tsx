04c09b39aec7ea5fcab771533faba23b9beacb55
sixthextinction <prithwish.nath@gmail.com>
2023-01-23 01:26:03 +0530

    added disable feeat

--- CustomButton.tsx
+++ CustomButton.tsx
@@ -1,10 +1,12 @@
+
 import React from "react";
 
 const CustomButton = () => {
   return (
     <button
       type="submit"
-      className="z-20 w-1/6 rounded-r-full bg-teal-500 py-2 px-4 font-bold text-white hover:bg-teal-700"
+      className="z-20 w-1/6 rounded-r-full bg-teal-500 py-2 px-4 font-bold text-white hover:bg-teal-700 disabled:bg-teal-200 disabled:text-gray-500"
+      disabled={submitDisabled || isMutating}
     >
       Send
     </button>
