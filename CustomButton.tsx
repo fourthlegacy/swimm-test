f38d3e1614132d6847fecac4f99c8ecad0746ce6
sixthextinction <prithwish.nath@gmail.com>
2023-01-23 01:03:26 +0530

    new button

--- CustomButton.tsx
+++ CustomButton.tsx
@@ -1,9 +1,20 @@
+let submitDisabled = false;
+let isMutating = false;
+
+
+
 import React from "react";
 
 const CustomButton = () => {
-  return <button>
-    Submit
-  </button>
+  return (
+    <button
+      type="submit"
+      className="z-20 w-1/6 rounded-r-full bg-teal-500 py-2 px-4 font-bold text-white hover:bg-teal-700 disabled:bg-teal-200 disabled:text-gray-500"
+      disabled={submitDisabled || isMutating}
+    >
+      Send
+    </button>
+  );
 };
 
 export default CustomButton;
